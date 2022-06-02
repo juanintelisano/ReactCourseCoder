4989e46813456697ce097126fb986d674c8b7417
juanintelisano <juanintelisano5@gmail.com>
2022-06-02 20:10:09 -0300

    desafio menu e-commerce Intelisano

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+const Nav = () => {
+    return (
+            <nav>
+                <h3>Desafio</h3>
+                <div class="links">
+                    <a href="#" class="link1">Home</a>
+                    <a href="#" class="link1">Location</a>
+                    <a href="#" class="link1">Contact</a>
+                </div>
+            </nav>
+    )
+}
+
+export default Nav
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import ALGO from 'ruta' (las rutas o paquetes estan en README)
 
+//   1- Traigo la librería de React
+import React from 'react'; 
+
+//   2-  Agarrar una aplicacion existente de REACT
+import ReactDOM from 'react-dom/client';
+
+//   3-  Traigo la libreria de ReactDOM
+/*             import './index.css';                */
+import App from './App';
+/*      import reportWebVitals from './reportWebVitals';            */
+
+//   4-  Agarrar una aplicacion existente de REACT
+
+
+//   5- Mostrar la app del DOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  /*              <React.StrictMode>            */
     <App />
-  </React.StrictMode>
+ /*               </React.StrictMode>           */ 
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+/*          reportWebVitals();             */ 
