0b3e6b68bb3a056a267b79530601c341e5e1a866
juanintelisano <juanintelisano5@gmail.com>
2022-06-07 19:33:00 -0300

    change

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ const Nav = () => {
     return (
             <nav>
                 <h3>Desafio</h3>
-                <div class="links">
+                <div className="links">
                     <a href="#" class="link1">Home</a>
                     <a href="#" class="link1">Location</a>
                     <a href="#" class="link1">Contact</a>
